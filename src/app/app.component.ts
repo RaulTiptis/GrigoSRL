ca79ec34b38b63e8eb0bd4d975b44f3dd46923a9
Ciumpi <raultiptis28@gmail.com>
2023-11-15 20:25:11 +0200

    main theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import bootstrap from '../main.server';
+
+function scripts(){
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+});
+}
+
 
 @Component({
   selector: 'app-root',
@@ -11,4 +39,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'grigoSRL';
+
+  ngOnInit() {
+    scripts();
+  }
 }
